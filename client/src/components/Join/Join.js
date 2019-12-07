841a0afa1c9ab646a155ae615f49f1c786b45d55
Alex Kozopolianski <42234681+alexkozopolianski@users.noreply.github.com>
2019-12-07 15:41:16 +0300

    Add files via upload

---
+++ client/src/components/Join/Join.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import './Join.css';
+
+const Join = () => {
+ const [name, setName] = useState('');
+ const [room, setRoom] = useState('');
+
+ return(
+     <div className="joinOuterContainer">
+         <div className="joinInnerContainer">
+             <h1 className="heading">Войдите</h1>
+             <div><input placeholder="Имя"
+             className="joinInput" type="text"
+             onChange={(event) => setName(event.target.value)} />
+             </div>
+             <div><input placeholder="Комната"
+             className="joinInput mt-20" type="text"
+             onChange={(event) => setRoom(event.target.value)} />
+             </div>
+             <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`} >
+                 <button type="submit" className="button mt-20">Войти</button>
+             </Link>
+         </div> 
+     </div>
+ )
+}
+
+export default Join;
